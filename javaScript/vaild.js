32372a32fd4171d4feeddd71952ad42b6d3c41ad
SurendraReddy12345 <129193595+SurendraReddy12345@users.noreply.github.com>
2023-08-15 12:19:38 +0530

    Add files via upload

---
+++ javaScript/vaild.js
@@ -0,0 +1,26 @@
+function validate(){
+    var username=document.getElementById("stdid");
+    var password=document.getElementById("pass");
+
+    if(username.value.trim()=="")
+    {
+        
+        document.getElementById("notvaild").style.visibility="visible";
+
+        return false;
+
+    }
+    else if(password.value.trim()==""){
+        
+        document.getElementById("notvaild1").style.visibility="visible";
+        return false;
+    }
+    else if(password.value.trim().length<6){
+        alert("password too short");
+        return false;
+    }
+    else{
+        true;
+    }
+
+}
